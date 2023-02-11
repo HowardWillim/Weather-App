d2abd423d67ee8825024ba0fdd705046ffbbaf03
HowardWillim <kurolimzz@gmail.com>
2023-02-11 20:57:58 +0700

    adding option for user to input location

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express=require("express");
 const https = require('https');
+const bodyParser = require("body-parser");
 const app =express();
 
-
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",function(req,res){
-    const urls ="https://api.openweathermap.org/data/2.5/weather?q=Pekanbaru&appid=961eeb7c6ea16cbbae322c0770b83e3b&units=metric"
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",function(req,res){
+
+
+    const query=req.body.CityName;
+    const appid="961eeb7c6ea16cbbae322c0770b83e3b";
+    const unit ="metric"
+
+
+    const urls ="https://api.openweathermap.org/data/2.5/weather?q=Pekanbaru&appid="+ appid+ "&units="+unit;
     https.get(urls,function(response){
         response.on("data",function(data){
             const weatherData = JSON.parse(data);
@@ -15,14 +27,8 @@ app.get("/",function(req,res){
             const icon =weatherData.weather[0].icon;
             const imageURl ="http://openweathermap.org/img/wn/"+icon+"@2x.png";
 
-
-
-
-
-
-
             res.write("<p>The weather is currently " + weatherDescription +"</p>")
-            res.write("<h1>The temperature in Pekanbaru is "+ temp +" degree celcius. </h1> ");
+            res.write("<h1>The temperature in "+query+" is "+ temp +" degree celcius. </h1> ");
             res.write("<img src="+imageURl+">");
             res.send();
         })
@@ -33,8 +39,6 @@ app.get("/",function(req,res){
     
 }
 )
-
-
 
 
 
